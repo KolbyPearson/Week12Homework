f3f06edb85969ebc37c9d983d30dc73a1780f606
Kolby Pearson <kolbyrpearson@gmail.com>
2022-05-13 07:02:24 -0500

    first commit

---
+++ src/main/java/unit/test/assignment/TestDemo.java
@@ -0,0 +1,25 @@
+package unit.test.assignment;
+
+import java.util.Random;
+
+public class TestDemo {
+
+	public int addPositive(int a, int b) {
+		if (a > 0 && b > 0) {
+			int sum = a + b;
+			return sum;
+		} else {
+			throw new IllegalArgumentException("Both Parameters must be positive!");
+		}	
+	}
+	
+	public int randomNumberSquared() {
+		int randomNumber = getRandomInt();
+		return randomNumber * randomNumber;
+ 	}
+	
+	int getRandomInt() {
+		  Random random = new Random();
+		  return random.nextInt(10) + 1;
+		}
+}
